311b2818027d00f4032ef9319cd1d2010917dbfb
alastair27 <alastair.howard@gmail.com>
2015-03-02 15:59:00 +0000

    Update initials.c

--- initials.c
+++ initials.c
@@ -8,9 +8,12 @@ int main(void)
     //gets the userś name
     string s = GetString(); 
     {
+        //prints the first initial
         printf("%c", toupper(s[0]));
         for (int i=0,n=strlen(s);i<n;i++)
+            //finds space before last name
             if (s[i] == 32)
+                //prints last initial
                 printf("%c",toupper(s[i+1]));
     }
     printf("\n");
